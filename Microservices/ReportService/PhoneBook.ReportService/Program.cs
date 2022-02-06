edaa7db735fc2ba0118cea49a5ea68cc04bab2dc
sezginclk <sezgincolak90@gmail.com>
2022-02-06 18:33:40 +0300

    AppSettings and Startup URL Changed

--- Microservices/ReportService/PhoneBook.ReportService/Program.cs
+++ Microservices/ReportService/PhoneBook.ReportService/Program.cs
@@ -20,7 +20,7 @@ namespace PhoneBook.ReportService
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>().UseUrls("http://*:5002");
                 });
     }
 }
